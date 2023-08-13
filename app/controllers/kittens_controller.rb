32183ef5dae8881c4b2468fdc8933950eaa339b5
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-08-12 22:56:03 -0700

    Add kitten#new and kitten#create method
    Included views html for new and _form
    to create new kittens

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -6,4 +6,24 @@ class KittensController < ApplicationController
   def show
     @kitten = Kitten.find(params[:id])
   end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kittens_param)
+    if @kitten.save
+      flash[:notice] = "Kitten successfully added"
+      redirect_to @kitten
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kittens_param
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
 end
